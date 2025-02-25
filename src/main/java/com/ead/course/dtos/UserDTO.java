3ace05c20a8f965f086b9d1da254c5d01dcc329c
igorgcf <igorTecnologia@outlook.com>
2025-02-25 19:38:32 -0300

    User DTO

---
+++ src/main/java/com/ead/course/dtos/UserDTO.java
@@ -0,0 +1,37 @@
+package com.ead.course.dtos;
+
+import com.ead.course.enums.UserStatus;
+import com.ead.course.enums.UserType;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+public class UserDTO {
+
+    private UUID id;
+    private String username;
+    private String email;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String oldPassword;
+
+    private String fullName;
+    private String phoneNumber;
+    private String cpf;
+    private String imageUrl;
+    private UserStatus userStatus;
+    private UserType userType;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private LocalDateTime creationDate;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private LocalDateTime lastUpdateDate;
+}
