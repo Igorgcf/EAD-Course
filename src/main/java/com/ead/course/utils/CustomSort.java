1520984a4f2eba8f418ba82c4f008cf260a8399f
igorgcf <igorTecnologia@outlook.com>
2025-02-25 19:48:24 -0300

    Custom Sort

---
+++ src/main/java/com/ead/course/utils/CustomSort.java
@@ -0,0 +1,13 @@
+package com.ead.course.utils;
+
+import java.util.List;
+
+public class CustomSort {
+
+    private List<Order> orders;
+
+    public static class Order{
+        private String direction;
+        private String property;
+    }
+}
