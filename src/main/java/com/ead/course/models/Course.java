1beb330e8195f149720b1c8505950b9943acbcc8
igorgcf <igorTecnologia@outlook.com>
2025-02-25 19:34:23 -0300

    Course Model

---
+++ src/main/java/com/ead/course/models/Course.java
@@ -0,0 +1,89 @@
+package com.ead.course.models;
+
+import com.ead.course.enums.Level;
+import com.ead.course.enums.Status;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.ToString;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "tb_course")
+public class Course implements Serializable {
+    private static final long SerialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, length = 170)
+    private String name;
+
+    @Column(nullable = false, length = 270)
+    private String description;
+
+    @Column
+    private String imageUrl;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    @Column(nullable = false)
+    private LocalDateTime creationDate;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    @Column(nullable = false)
+    private LocalDateTime lastUpdateDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Level courseLevel;
+
+    @Column(nullable = false)
+    private UUID instructorId;
+
+    @Fetch(FetchMode.SUBSELECT)
+    @ToString.Exclude
+    @OneToMany(mappedBy = "course", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    List<Module> modules = new ArrayList<>();
+
+    @OneToMany(mappedBy = "course", fetch = FetchType.LAZY)
+    private List<CourseUser> courseUsers =  new ArrayList<>();
+
+    public Course(){
+    }
+
+    public Course(UUID id, String name, String description, String imageUrl, LocalDateTime creationDate, LocalDateTime lastUpdateDate,
+                  Status status, Level courseLevel, UUID instructorId) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.creationDate = creationDate;
+        this.lastUpdateDate = lastUpdateDate;
+        this.status = status;
+        this.courseLevel = courseLevel;
+        this.instructorId = instructorId;
+    }
+}
