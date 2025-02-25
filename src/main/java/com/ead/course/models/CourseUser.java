67bf1c3cd04e6b0126b6be4ca017a6dba8317dec
igorgcf <igorTecnologia@outlook.com>
2025-02-25 19:34:48 -0300

    Course User Model

---
+++ src/main/java/com/ead/course/models/CourseUser.java
@@ -0,0 +1,32 @@
+package com.ead.course.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "tb_course_user")
+public class CourseUser implements Serializable {
+    private static final long SerialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Course course;
+
+    @Column(nullable = false)
+    private UUID userId;
+
+}
