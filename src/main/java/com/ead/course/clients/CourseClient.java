e7badd6d1fa64a28a78cab88f697d3002568b5e7
igorgcf <igorTecnologia@outlook.com>
2025-02-27 19:35:17 -0300

    Course Client

--- src/main/java/com/ead/course/clients/CourseClient.java
+++ src/main/java/com/ead/course/clients/CourseClient.java
@@ -1,10 +1,13 @@
 package com.ead.course.clients;
 
+import com.ead.course.dtos.CourseDTO;
 import com.ead.course.dtos.ResponsePageDTO;
 import com.ead.course.dtos.UserDTO;
+import com.ead.course.models.Course;
 import com.ead.course.services.UtilsService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,6 +22,9 @@ import java.util.UUID;
 @Log4j2
 @Component
 public class CourseClient {
+
+    @Value("${ead.api.url.authuser}")
+    String REQUEST_URL_AUTHUSER;
 
     @Autowired
     private RestTemplate restTemplate;
@@ -46,5 +52,35 @@ public class CourseClient {
         log.info("Ending request /users courseId {} ", courseId );
 
         return Page.empty();
+    }
+
+    public UserDTO findById(UUID userId) {
+
+        String url = service.createUrl(userId);
+
+        log.debug("Request URL: {} ", url);
+        log.info("Request URL: {} ", url);
+
+        try {
+            ResponseEntity<UserDTO> result = restTemplate.exchange(url, HttpMethod.GET, null, UserDTO.class);
+
+            if (result.getBody() != null) {
+                log.debug("Response: {} ", result.getBody());
+                return result.getBody();
+            }
+            }catch(HttpStatusCodeException e){
+                log.error("Error request /users {} ", e);
+            }
+            log.info("Ending request /users userId {} ", userId );
+
+            return null;
+    }
+
+    public void postSubscriptionUserInCourse(Course entity, UUID userId){
+
+        String url = REQUEST_URL_AUTHUSER + "/users/" + userId + "/courses/subscription";
+        CourseDTO dto = new CourseDTO();
+        dto = new CourseDTO(entity);
+        restTemplate.postForObject(url, dto, String.class);
     }
 }
