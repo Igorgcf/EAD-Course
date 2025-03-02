daa6fdde9068a0fb96e19007f74796baeb1312e8
igorgcf <igorTecnologia@outlook.com>
2025-03-02 11:55:23 -0300

    Course User Repository

--- src/main/java/com/ead/course/repositories/CourseUserRepository.java
+++ src/main/java/com/ead/course/repositories/CourseUserRepository.java
@@ -2,10 +2,16 @@ package com.ead.course.repositories;
 
 import com.ead.course.models.CourseUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CourseUserRepository extends JpaRepository<CourseUser, UUID> {
 
     boolean existsByCourseIdAndUserId(UUID courseId, UUID userId);
+
+    @Query(value = "SELECT * FROM tb_course_user WHERE course_id = :courseId", nativeQuery = true)
+    List<CourseUser> findAllCourseUserIntoCourse(@Param("courseId") UUID courseId);
 }
