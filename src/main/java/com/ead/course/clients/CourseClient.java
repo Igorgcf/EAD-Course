bd8ae4a109ef53e9e813ab1d9867aeac61858b28
igorgcf <igorTecnologia@outlook.com>
2025-03-02 11:57:22 -0300

    Course Client

--- src/main/java/com/ead/course/clients/CourseClient.java
+++ src/main/java/com/ead/course/clients/CourseClient.java
@@ -5,6 +5,7 @@ import com.ead.course.dtos.ResponsePageDTO;
 import com.ead.course.dtos.UserDTO;
 import com.ead.course.models.Course;
 import com.ead.course.services.UtilsService;
+import com.ead.course.services.exceptions.ResourceNotFoundException;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,6 +15,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
@@ -32,24 +34,25 @@ public class CourseClient {
     @Autowired
     private UtilsService service;
 
-    public Page<UserDTO> findAllUsersByCourse(UUID courseId, Pageable pageable){
+    public Page<UserDTO> findAllUsersByCourse(UUID courseId, Pageable pageable) {
 
         String url = service.createUrl(courseId, pageable);
 
         log.debug("Request URL: {} ", url);
         log.info("Request URL: {} ", url);
 
-        try{
-            ParameterizedTypeReference<ResponsePageDTO<UserDTO>> responseType = new ParameterizedTypeReference<ResponsePageDTO<UserDTO>>() {};
+        try {
+            ParameterizedTypeReference<ResponsePageDTO<UserDTO>> responseType = new ParameterizedTypeReference<ResponsePageDTO<UserDTO>>() {
+            };
             ResponseEntity<ResponsePageDTO<UserDTO>> result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
-            if(result.getBody() != null){
+            if (result.getBody() != null) {
                 log.debug("Response Number of Elements: {} ", result.getBody().getContent().size());
                 return result.getBody();
             }
-        }catch(HttpStatusCodeException e){
+        } catch (HttpStatusCodeException e) {
             log.error("Error request /users {} ", e);
         }
-        log.info("Ending request /users courseId {} ", courseId );
+        log.info("Ending request /users courseId {} ", courseId);
 
         return Page.empty();
     }
@@ -68,13 +71,13 @@ public class CourseClient {
                 log.debug("Response: {} ", result.getBody());
                 return result.getBody();
             }
-            }catch(HttpStatusCodeException e){
-                log.error("Error request /users {} ", e);
-            }
-            log.info("Ending request /users userId {} ", userId );
+        } catch (HttpClientErrorException.NotFound e) {
+            throw new ResourceNotFoundException("User Id or Instructor Id not found: " + userId);
+        }
 
-            return null;
+        return null;
     }
+
 
     public void postSubscriptionUserInCourse(Course entity, UUID userId){
 
