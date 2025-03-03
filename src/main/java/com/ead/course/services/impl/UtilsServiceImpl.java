f644b4d14213c915348586da95d94dce06f8e3c7
igorgcf <igorTecnologia@outlook.com>
2025-03-03 20:01:53 -0300

    Utils Service Impl

--- src/main/java/com/ead/course/services/impl/UtilsServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/UtilsServiceImpl.java
@@ -1,6 +1,7 @@
 package com.ead.course.services.impl;
 
 import com.ead.course.services.UtilsService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -9,17 +10,18 @@ import java.util.UUID;
 @Service
 public class UtilsServiceImpl implements UtilsService {
 
-    String REQUEST_URI = "http://localhost:8087";
+    @Value("${ead.api.url.authuser}")
+    String REQUEST_URL_AUTHUSER;
 
     @Override
     public String createUrl(UUID courseId, Pageable pageable) {
-        return REQUEST_URI + "/users?courseId=" + courseId + "&page=" + pageable.getPageNumber() + "&size="
+        return REQUEST_URL_AUTHUSER + "/users?courseId=" + courseId + "&page=" + pageable.getPageNumber() + "&size="
                 + pageable.getPageSize() + "&sort=" + pageable.getSort().toString().replaceAll(": ", ",");
     }
 
     @Override
     public String createUrl(UUID userId) {
-        return REQUEST_URI + "/users/" + userId;
+        return REQUEST_URL_AUTHUSER + "/users/" + userId;
 
     }
 
