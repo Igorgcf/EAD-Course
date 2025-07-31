457e0209c10072b6b518d27e900e1dec595b5473
igorgcf <igorTecnologia@outlook.com>
2025-07-31 01:56:36 -0300

    Course User Service

--- src/main/java/com/ead/course/services/CourseUserService.java
+++ src/main/java/com/ead/course/services/CourseUserService.java
@@ -2,10 +2,16 @@ package com.ead.course.services;
 
 import com.ead.course.dtos.CourseUserDTO;
 import com.ead.course.dtos.UserDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.UUID;
 
 public interface CourseUserService {
 
+    Page<CourseUserDTO> findAllPaged(Pageable pageable);
+
     CourseUserDTO saveAndSendSubscriptionUserInCourse(UUID courseId, UserDTO dto);
+
+    void deleteCourseUserByUser(UUID userId);
 }
