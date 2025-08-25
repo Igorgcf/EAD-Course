819abf8f7097c05aaba3f5a2e08ef2ab845b373a
igorgcf <igorTecnologia@outlook.com>
2025-08-25 15:18:31 -0300

    User Entity

--- src/main/java/com/ead/course/models/User.java
+++ src/main/java/com/ead/course/models/User.java
@@ -1,13 +1,18 @@
 package com.ead.course.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -17,7 +22,26 @@ public class User implements Serializable {
     private static final long SerialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
+    @Column(nullable = false, unique = true, length = 50)
+    private String email;
+
+    @Column(nullable = false, length = 150)
+    private String fullName;
+
+    @Column(nullable = false)
+    private String userStatus;
+
+    @Column(nullable = false)
+    private String userType;
+
+    @Column(length = 50)
+    private String cpf;
+
+    @Column
+    private String imageUrl;
+
+    @ManyToMany(mappedBy = "users", fetch = FetchType.LAZY)
+    List<Course> courses = new ArrayList<>();
 }
