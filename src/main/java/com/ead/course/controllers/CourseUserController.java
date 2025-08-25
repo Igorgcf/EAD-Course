7ba199163f03c5b51150d606e39f08f7cdf50ca4
igorgcf <igorTecnologia@outlook.com>
2025-08-25 11:05:18 -0300

    Course User Controller

--- src/main/java/com/ead/course/controllers/CourseUserController.java
+++ src/main/java/com/ead/course/controllers/CourseUserController.java
@@ -2,20 +2,12 @@ package com.ead.course.controllers;
 
 import com.ead.course.services.CourseService;
 import com.ead.course.services.UserService;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
@@ -30,9 +22,9 @@ public class CourseUserController {
     private CourseService courseService;
 
     @PostMapping(value = "/courses/{courseId}/users/subscription")
-    public ResponseEntity<Object> saveAndSendSubscriptionUserInCourse(@PathVariable(value = "courseId") UUID courseId,
-                                                                             @Validated(UserDTO.UserView.RegistrationPost.class)
-                                                                             @RequestBody @Valid UserDTO dto) {
+    public ResponseEntity<Object> saveAndSendSubscriptionUserInCourse(@PathVariable(value = "courseId") UUID courseId
+                                                                             //@Validated(UserDTO.UserView.RegistrationPost.class)
+                                                                             ) {
         return ResponseEntity.status(HttpStatus.CREATED).body("");
     }
 }
