b0e8e32abcf9f2f35cde89abafe9ccc1d7e33306
igorgcf <igorTecnologia@outlook.com>
2025-08-25 15:17:17 -0300

    User Service

--- src/main/java/com/ead/course/services/UserService.java
+++ src/main/java/com/ead/course/services/UserService.java
@@ -1,4 +1,12 @@
 package com.ead.course.services;
 
+import com.ead.course.dtos.UserDTO;
+import com.ead.course.models.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+
 public interface UserService {
+
+    Page<UserDTO> findAllPaged(Specification<User> spec, Pageable pageable);
 }
