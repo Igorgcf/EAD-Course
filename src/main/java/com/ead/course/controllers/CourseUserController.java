a3e6de169d20c2be74d363371828c358ae16850b
igorgcf <igorTecnologia@outlook.com>
2025-08-25 10:56:31 -0300

    Course User Controller

--- src/main/java/com/ead/course/controllers/CourseUserController.java
+++ src/main/java/com/ead/course/controllers/CourseUserController.java
@@ -1,9 +1,8 @@
 package com.ead.course.controllers;
 
-import com.ead.course.clients.CourseClient;
-import com.ead.course.dtos.CourseUserDTO;
 import com.ead.course.dtos.UserDTO;
-import com.ead.course.services.CourseUserService;
+import com.ead.course.services.CourseService;
+import com.ead.course.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -14,7 +13,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,38 +26,22 @@ import java.util.UUID;
 public class CourseUserController {
 
     @Autowired
-    private CourseClient courseClient;
+    private UserService service;
 
-    @Autowired
-    private CourseUserService service;
-
-    @GetMapping(value = "/courses/users")
-    public ResponseEntity<Page<CourseUserDTO>> findAllPaged(Pageable pageable) {
-
-        Page<CourseUserDTO> page = service.findAllPaged(pageable);
-        return ResponseEntity.ok().body(page);
-    }
-
+    private CourseService courseService;
+    
     @GetMapping(value = "/courses/{courseId}/users")
     public ResponseEntity<Page<UserDTO>> findAllUserByCourse(@PageableDefault(page = 0, size = 12, sort = "id", direction = Sort.Direction.ASC) Pageable pageable,
                                                              @PathVariable(value = "courseId") UUID courseId) {
 
-        Page<UserDTO> page = courseClient.findAllUsersByCourse(courseId, pageable);
+        Page<UserDTO> page = null;
         return ResponseEntity.ok().body(page);
     }
 
     @PostMapping(value = "/courses/{courseId}/users/subscription")
-    public ResponseEntity<CourseUserDTO> saveAndSendSubscriptionUserInCourse(@PathVariable(value = "courseId") UUID courseId,
+    public ResponseEntity<Object> saveAndSendSubscriptionUserInCourse(@PathVariable(value = "courseId") UUID courseId,
                                                                              @Validated(UserDTO.UserView.RegistrationPost.class)
                                                                              @RequestBody @Valid UserDTO dto) {
-
-        CourseUserDTO response = service.saveAndSendSubscriptionUserInCourse(courseId, dto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
-    }
-
-    @DeleteMapping(value = "/courses/users/{userId}")
-    public ResponseEntity<Object> deleteCourseUser(@PathVariable(value = "userId") UUID userId) {
-        service.deleteCourseUserByUser(userId);
-        return ResponseEntity.ok().body("User successfully unsubscribed from the course.");
+        return ResponseEntity.status(HttpStatus.CREATED).body("");
     }
 }
