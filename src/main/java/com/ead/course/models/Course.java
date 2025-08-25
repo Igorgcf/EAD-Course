601bf7b45fd7f990ddf056d102ab36ce442c425a
igorgcf <igorTecnologia@outlook.com>
2025-08-25 10:16:48 -0300

    Course Entity

--- src/main/java/com/ead/course/models/Course.java
+++ src/main/java/com/ead/course/models/Course.java
@@ -8,7 +8,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -68,9 +67,6 @@ public class Course implements Serializable {
     @OneToMany(mappedBy = "course", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     List<Module> modules = new ArrayList<>();
 
-    @OneToMany(mappedBy = "course", fetch = FetchType.LAZY)
-    private List<CourseUser> courseUsers =  new ArrayList<>();
-
     public Course(){
     }
 
@@ -85,9 +81,5 @@ public class Course implements Serializable {
         this.status = status;
         this.courseLevel = courseLevel;
         this.instructorId = instructorId;
-    }
-
-    public CourseUser convertToCourseUser(UUID userId){
-        return new CourseUser(null, this, userId);
     }
 }
