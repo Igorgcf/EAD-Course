fa41b45e5df329f6b8e61928eafca328fd97a32b
Igorgcf <igorTecnologia@outlook.com>
2026-01-14 10:56:18 -0300

    Course User Service Impl

--- src/main/java/com/ead/course/services/impl/CourseUserServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/CourseUserServiceImpl.java
@@ -1,9 +1,12 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.dtos.CourseDTO;
+import com.ead.course.dtos.NotificationDTO;
 import com.ead.course.dtos.UserDTO;
 import com.ead.course.enums.UserStatus;
 import com.ead.course.models.Course;
 import com.ead.course.models.User;
+import com.ead.course.publishers.NotificationPublisher;
 import com.ead.course.repositories.CourseRepository;
 import com.ead.course.repositories.UserRepository;
 import com.ead.course.services.CourseUserService;
@@ -26,6 +29,9 @@ public class CourseUserServiceImpl implements CourseUserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private NotificationPublisher publisher;
 
     @Transactional
     @Override
@@ -53,6 +59,31 @@ public class CourseUserServiceImpl implements CourseUserService {
         log.debug("Subscription saved {}, {}", entity.getId(), dto.getId());
         log.info("Subscription saved successfully courseId: {}, userId: {}", entity.getId(),
                 (dto.getId() != null ? dto.getId() : "No user id provided"));
+    }
+
+    @Transactional
+    @Override
+    public void saveSubscriptionUserInCourseAndSendNotification(CourseDTO courseDto, UserDTO userDto) {
+
+        saveSubscriptionUserInCourse(courseDto.getId(), userDto);
+
+
+        try{
+        NotificationDTO notificationDTO = new NotificationDTO();
+        copyDtoToEntity(notificationDTO, courseDto, userDto);
+        publisher.publishNotificationCommand(notificationDTO);
+        }catch(Exception e){
+            log.warn("Error sending notification!");
+        }
+    }
+
+    void copyDtoToEntity(NotificationDTO notificationDTO, CourseDTO courseDto, UserDTO userDto){
+
+        notificationDTO.setTitle("Welcome to the course: " + courseDto.getName());
+        notificationDTO.setUserId(userDto.getId());
+        notificationDTO.setMessage("Subscribed to course successfully! Course name: " + courseDto.getName());
+
+
     }
 
 }
