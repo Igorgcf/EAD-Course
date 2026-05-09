618bcd730212160e15caa8f7f78935decc86a62d
Igorgcf <igorTecnologia@outlook.com>
2026-05-09 18:57:17 -0300

    User Controller

--- src/main/java/com/ead/course/controllers/UserController.java
+++ src/main/java/com/ead/course/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class UserController {
     @Autowired
     private UserService service;
 
+    @PreAuthorize("hasAnyRole('INSTRUCTOR')")
     @GetMapping
     public ResponseEntity<Page<UserDTO>> findAll(Pageable pageable){
 
